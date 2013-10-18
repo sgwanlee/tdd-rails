f660196c4768c99201ecc85c60c0261df4fdaa83
Seonggwan Lee <seonggwan.lee@gmail.com>
2013-10-18 15:52:43 +0900

    Init - modified Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TddRails::Application.config.secret_key_base = '48f1b57914819e4a419c68a74a0ada16fff139af5fb96d7fa01438902df82548c48f0dbedb4db447df0da0332b5ec0075ef9840170515f45ada84ac953de4dca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TddRails::Application.config.session_store :cookie_store, key: '_tdd-rails_session'
